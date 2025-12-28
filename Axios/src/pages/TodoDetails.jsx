db02a64893205d446108b608bea034ee65447e8e
Pravallika Thupakula <127658780+pravallika1432@users.noreply.github.com>
2025-12-28 17:33:58 +0530

    Initial commit

---
+++ Axios/src/pages/TodoDetails.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import { getTodoById } from "../api/todoService";
+
+const TodoDetails = () => {
+  const { id } = useParams();
+  const [todo, setTodo] = useState(null);
+
+  useEffect(() => {
+    getTodoById(id).then((data) => setTodo(data));
+  }, [id]);
+
+  if (!todo) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h2>Todo Details</h2>
+      <p><b>ID:</b> {todo.id}</p>
+      <p><b>Title:</b> {todo.title}</p>
+      <p><b>Status:</b> {todo.completed ? "Completed" : "Pending"}</p>
+
+      <Link to="/">⬅ Back to Todo List</Link>
+    </div>
+  );
+};
+
+export default TodoDetails;
